31534b4c02efd871a73dcf24b6d0521e8ad892b2
gaotero05 <gaotero05@gmail.com>
2026-04-26 02:40:53 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -7,7 +7,7 @@ import Contact from "./pages/Contact";
 
 function App() {
  return (
-  <BrowserRouter>
+  <HashRouter>
    <Navbar />
     <Routes>
      <Route path="/" element={<Home />} />
@@ -15,7 +15,7 @@ function App() {
      <Route path="/projects" element={<Projects />} />
      <Route path="/contact" element={<Contact />} />
     </Routes>
-  </BrowserRouter>
+   </HashRouter>
  );
 }
 
